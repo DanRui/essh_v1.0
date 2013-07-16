79972e8577d59d39fa344597a1654f9e0c3947d8
eryanwcp@gmail.com <eryanwcp@gmail.com>
2013-07-16 13:16:30 +0800

    代码生成器调用

---
+++ test/test/codegen/CodeGenMain.java
@@ -0,0 +1,36 @@
+package test.codegen;
+
+import com.eryansky.codegen.build.Builder;
+import com.eryansky.codegen.db.DataSource;
+import com.eryansky.codegen.db.DbConnection;
+import com.eryansky.codegen.db.DbFactory;
+import com.eryansky.codegen.vo.Table;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CodeGenMain {
+	private List<Table> tables = null;
+	private Builder builder = null;
+	DataSource db = null;
+	String t = "t_%";//user_info
+	Table table = null;
+
+	@Before
+	public void before() {
+		try {
+			db = DbFactory.create(new DbConnection().getConn());
+			tables = db.getTables(t);
+			builder = new Builder(tables);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void buildTest() {
+		builder.build();
+	}
+
+}
