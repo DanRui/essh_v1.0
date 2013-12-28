f69e606adc2af3aa335eb53f022ded0d0a45510b
eryanwcp@gmail.com <eryanwcp@gmail.com>
2013-12-28 21:49:11 +0800

    1.升级EasyUI至1.3.5        2.highchart图表下载支持        3.新增日志管理，安全日志、操作日志。        4.列表不仅修改以及其它修改

--- src/com/eryansky/utils/AppConstants.java
+++ src/com/eryansky/utils/AppConstants.java
@@ -3,6 +3,7 @@ package com.eryansky.utils;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.eryansky.common.utils.io.PropertiesLoader;
 import com.eryansky.core.security.SessionInfo;
 
 /**
@@ -20,8 +21,32 @@ public class AppConstants {
 	 * 修改用户密码 个人(不需要输入原始密码)
 	 */
 	public static final String USER_UPDATE_PASSWORD_NO = "0";
-	
 
-	
+    private static PropertiesLoader config = null;
 
+    /**
+     * 配置文件路径
+     */
+    public static final String CONFIG_FILE_PATH = "config.properties";
+    /**
+     * 配置文件日志保留时间 key
+     */
+    public static final String CONFIG_LOGKEEPTIME = "logKeepTime";
+
+    /**
+     * 配置文件(config.properties)
+     */
+    public static PropertiesLoader getConfig() {
+        if(config == null){
+            config = new PropertiesLoader(CONFIG_FILE_PATH);
+        }
+        return config;
+    }
+
+    /**
+     * 日志保留时间 天(默认值:30).
+     */
+    public static int getLogKeepTime(){
+        return AppConstants.getConfig().getInteger(CONFIG_LOGKEEPTIME,30);
+    }
 }
