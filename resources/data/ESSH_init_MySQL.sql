93b73aa47b3edb6a9cc1044bd89e2b86269139f1
eryanwcp@gmail.com <eryanwcp@gmail.com>
2014-03-29 21:46:17 +0800

    * 2014-3-29        1.新增字典标签功能支持        2.bug管理添加标题自定义颜色        3.easyui扩展comboboxRequired校验类型，支持combobox为空校验

--- resources/data/ESSH_init_MySQL.sql
+++ resources/data/ESSH_init_MySQL.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50153
 File Encoding         : 65001
 
-Date: 2013-11-17 18:57:44
+Date: 2014-03-29 21:45:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -41,11 +41,13 @@ CREATE TABLE `t_base_organ` (
   UNIQUE KEY `NAME` (`NAME`),
   KEY `FKFE2373CE3E535456` (`PARENT_ID`),
   CONSTRAINT `FKFE2373CE3E535456` FOREIGN KEY (`PARENT_ID`) REFERENCES `t_base_organ` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_base_organ
 -- ----------------------------
+INSERT INTO `t_base_organ` VALUES ('7', '2014-03-29 21:43:39', 'admin', '0', null, null, '0', '', '', '', null, '尔演科技', '1', '', '00', '0', null);
+INSERT INTO `t_base_organ` VALUES ('8', '2014-03-29 21:43:51', 'admin', '0', null, null, '0', '', '', '', null, '软件部门', '2', '', '001', '1', '7');
 
 -- ----------------------------
 -- Table structure for t_base_resource
@@ -71,26 +73,31 @@ CREATE TABLE `t_base_resource` (
   PRIMARY KEY (`ID`),
   KEY `FKFCD7B111A886F349` (`PARENT_ID`),
   CONSTRAINT `FKFCD7B111A886F349` FOREIGN KEY (`PARENT_ID`) REFERENCES `t_base_resource` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_base_resource
 -- ----------------------------
 INSERT INTO `t_base_resource` VALUES ('1', null, null, '0', '2013-11-12 22:12:42', 'admin', '1', '', null, 'icon-application', '', '权限管理', '1', '0', '', null);
-INSERT INTO `t_base_resource` VALUES ('2', null, null, '0', '2013-11-12 22:17:18', 'admin', '3', '', null, 'icon-folder', '', '资源管理', '2', '0', '/base/resource.action', '1');
-INSERT INTO `t_base_resource` VALUES ('3', '2013-11-12 22:13:42', 'admin', '0', null, null, '0', '', null, 'icon-group', '', '角色管理', '3', '0', '/base/role.action', '1');
-INSERT INTO `t_base_resource` VALUES ('4', '2013-11-12 22:14:10', 'admin', '0', null, null, '0', '', null, 'icon-group', '', '机构管理', '4', '0', '/base/organ.action', '1');
-INSERT INTO `t_base_resource` VALUES ('5', '2013-11-12 22:14:28', 'admin', '0', null, null, '0', '', null, 'icon-user', '', '用户管理', '5', '0', '/base/user.action', '1');
+INSERT INTO `t_base_resource` VALUES ('2', null, null, '0', '2013-12-28 16:17:38', 'admin', '6', '', null, 'icon-folder', '', '资源管理', '2', '0', '/base/resource.action', '1');
+INSERT INTO `t_base_resource` VALUES ('3', '2013-11-12 22:13:42', 'admin', '0', '2013-12-22 00:01:53', 'admin', '3', '', null, 'icon-group', '', '角色管理', '3', '0', '/base/role.action', '1');
+INSERT INTO `t_base_resource` VALUES ('4', '2013-11-12 22:14:10', 'admin', '0', '2013-12-28 19:25:00', 'admin', '3', '', null, 'icon-group', '', '机构管理', '4', '0', '/base/organ.action', '1');
+INSERT INTO `t_base_resource` VALUES ('5', '2013-11-12 22:14:28', 'admin', '0', '2013-12-28 19:19:03', 'admin', '1', '', null, 'icon-user', '', '用户管理', '5', '0', '/base/user.action', '1');
 INSERT INTO `t_base_resource` VALUES ('6', '2013-11-12 22:14:54', 'admin', '0', '2013-11-12 22:15:18', 'admin', '1', '', null, 'icon-application', '', '系统配置', '6', '0', '', null);
-INSERT INTO `t_base_resource` VALUES ('7', '2013-11-12 22:15:13', 'admin', '0', '2013-11-12 22:28:36', 'admin', '1', '', null, 'icon-book', '', '字典类型', '7', '0', '/sys/dictionary-type.action', '6');
-INSERT INTO `t_base_resource` VALUES ('8', '2013-11-12 22:15:40', 'admin', '0', '2013-11-12 22:28:43', 'admin', '1', '', null, 'icon-ipod', '', '数据字典', '8', '0', '/sys/dictionary.action', '6');
-INSERT INTO `t_base_resource` VALUES ('9', '2013-11-12 22:15:57', 'admin', '0', null, null, '0', '', null, 'icon-bug', '', 'bug管理', '9', '0', '/sys/bug.action', '6');
-INSERT INTO `t_base_resource` VALUES ('11', '2013-11-16 23:45:54', 'admin', '0', '2013-11-17 00:40:32', 'eryan', '3', 'bug:add', null, '', '/sys/bug!input*;/sys/bug!save*', '新增', '10', '0', '', '9');
-INSERT INTO `t_base_resource` VALUES ('16', '2013-11-17 00:10:23', 'admin', '0', '2013-11-17 00:18:48', 'eryan', '1', 'bug:edit', null, '', '/sys/bug!save*', '编辑', '11', '0', '', '9');
-INSERT INTO `t_base_resource` VALUES ('17', '2013-11-17 00:11:05', 'admin', '0', '2013-11-17 00:12:40', 'admin', '1', 'bug:importExcel', null, '', '/sys/bug!importExcel*', 'Excel导入', '12', '0', '', '9');
-INSERT INTO `t_base_resource` VALUES ('18', '2013-11-17 00:13:35', 'admin', '0', null, null, '0', 'bug:exportExcel', null, '', '/sys/bug!exportExcel*', 'Excel导出', '13', '0', '', '9');
-INSERT INTO `t_base_resource` VALUES ('19', '2013-11-17 00:14:32', 'admin', '0', null, null, '0', 'bug:remove', null, '', '/sys/bug!delete*;/sys/bug!remove*', '批量删除', '14', '0', '', '9');
-INSERT INTO `t_base_resource` VALUES ('20', '2013-11-17 00:46:09', 'eryan', '0', null, null, '0', 'bug:view', null, '', '/sys/bug!view*', '查看', '15', '0', '', '9');
+INSERT INTO `t_base_resource` VALUES ('7', '2013-11-12 22:15:13', 'admin', '0', '2013-12-21 21:02:05', 'admin', '2', '', null, 'icon-book', '', '字典类型', '7', '0', '/sys/dictionary-type.action', '6');
+INSERT INTO `t_base_resource` VALUES ('8', '2013-11-12 22:15:40', 'admin', '0', '2013-12-28 19:25:41', 'admin', '5', '', null, 'icon-ipod', '', '数据字典', '8', '0', '/sys/dictionary.action', '6');
+INSERT INTO `t_base_resource` VALUES ('9', '2013-11-12 22:15:57', 'admin', '0', '2014-03-29 18:43:23', 'admin', '2', '', null, 'icon-bug', '', '内容管理', '9', '0', '/sys/bug.action', '6');
+INSERT INTO `t_base_resource` VALUES ('11', '2013-11-16 23:45:54', 'admin', '0', '2013-12-21 19:57:45', 'admin', '5', 'bug:add', null, '', '/sys/bug!input*;/sys/bug!save*', '新增', '10', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('16', '2013-11-17 00:10:23', 'admin', '0', '2013-11-17 20:16:33', 'admin', '2', 'bug:edit', null, '', '/sys/bug!save*', '编辑', '11', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('17', '2013-11-17 00:11:05', 'admin', '0', '2013-11-17 20:16:38', 'admin', '2', 'bug:importExcel', null, '', '/sys/bug!importExcel*', 'Excel导入', '12', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('18', '2013-11-17 00:13:35', 'admin', '0', '2013-11-17 20:16:41', 'admin', '1', 'bug:exportExcel', null, '', '/sys/bug!exportExcel*', 'Excel导出', '13', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('19', '2013-11-17 00:14:32', 'admin', '0', '2013-11-17 20:16:45', 'admin', '1', 'bug:remove', null, '', '/sys/bug!delete*;/sys/bug!remove*', '批量删除', '14', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('20', '2013-11-17 00:46:09', 'eryan', '0', '2013-11-17 20:16:50', 'admin', '1', 'bug:view', null, '', '/sys/bug!view*', '查看', '15', '1', '', '9');
+INSERT INTO `t_base_resource` VALUES ('21', '2013-12-08 17:26:38', 'admin', '0', '2013-12-08 17:27:03', 'admin', '1', '', null, 'icon-monitor', '/sys/log*', '日志管理', '16', '0', '/sys/log.action', '6');
+INSERT INTO `t_base_resource` VALUES ('22', '2014-01-05 15:07:01', 'admin', '0', '2014-03-29 21:40:06', 'admin', '1', '', null, 'icon-application', '', '演示', '17', '0', '', null);
+INSERT INTO `t_base_resource` VALUES ('23', '2014-01-05 15:08:02', 'admin', '0', '2014-03-29 21:40:14', 'admin', '4', '', null, '', '', '数据字典', '18', '0', '/fileRedirect.action?toPage=demo/dictionarydemo.jsp&amp;amp;name=ok', '22');
+INSERT INTO `t_base_resource` VALUES ('24', '2014-03-29 21:40:36', 'admin', '0', '2014-03-29 21:43:05', 'admin', '3', '', null, '', '', 'Quartz Cron', '19', '0', '/fileRedirect.action?toPage=demo/cron.jsp', '22');
+INSERT INTO `t_base_resource` VALUES ('25', '2014-03-29 21:41:31', 'admin', '0', '2014-03-29 21:42:42', 'admin', '2', '', null, '', '', 'easyui-my97', '20', '0', '/fileRedirect.action?toPage=demo/my97demo.jsp', '22');
 
 -- ----------------------------
 -- Table structure for t_base_role
@@ -157,7 +164,7 @@ CREATE TABLE `t_base_user` (
   UNIQUE KEY `LOGIN_NAME` (`LOGIN_NAME`),
   KEY `FKBDE2DA4E7AEFAE74` (`DEFAULT_ORGANID`),
   CONSTRAINT `FKBDE2DA4E7AEFAE74` FOREIGN KEY (`DEFAULT_ORGANID`) REFERENCES `t_base_organ` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_base_user
@@ -173,8 +180,8 @@ CREATE TABLE `t_base_user_organ` (
   `ORGAN_ID` bigint(20) NOT NULL,
   KEY `FK1F9964C01162FD8F` (`ORGAN_ID`),
   KEY `FK1F9964C0712445C5` (`USER_ID`),
-  CONSTRAINT `FK1F9964C0712445C5` FOREIGN KEY (`USER_ID`) REFERENCES `t_base_user` (`ID`),
-  CONSTRAINT `FK1F9964C01162FD8F` FOREIGN KEY (`ORGAN_ID`) REFERENCES `t_base_organ` (`ID`)
+  CONSTRAINT `FK1F9964C01162FD8F` FOREIGN KEY (`ORGAN_ID`) REFERENCES `t_base_organ` (`ID`),
+  CONSTRAINT `FK1F9964C0712445C5` FOREIGN KEY (`USER_ID`) REFERENCES `t_base_user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -190,8 +197,8 @@ CREATE TABLE `t_base_user_resource` (
   `RESOURCE_ID` bigint(20) NOT NULL,
   KEY `FKD8C9C2DF712445C5` (`USER_ID`),
   KEY `FKD8C9C2DF76B5CD65` (`RESOURCE_ID`),
-  CONSTRAINT `FKD8C9C2DF76B5CD65` FOREIGN KEY (`RESOURCE_ID`) REFERENCES `t_base_resource` (`ID`),
-  CONSTRAINT `FKD8C9C2DF712445C5` FOREIGN KEY (`USER_ID`) REFERENCES `t_base_user` (`ID`)
+  CONSTRAINT `FKD8C9C2DF712445C5` FOREIGN KEY (`USER_ID`) REFERENCES `t_base_user` (`ID`),
+  CONSTRAINT `FKD8C9C2DF76B5CD65` FOREIGN KEY (`RESOURCE_ID`) REFERENCES `t_base_resource` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -230,12 +237,14 @@ CREATE TABLE `t_sys_bug` (
   `CONTENT` longtext,
   `TITLE` varchar(255) DEFAULT NULL,
   `TYPE` varchar(36) DEFAULT NULL,
+  `COLOR` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_sys_bug
 -- ----------------------------
+INSERT INTO `t_sys_bug` VALUES ('17', '2014-03-29 20:27:44', 'admin', '0', null, null, '0', '欢迎使用', '欢迎使用', 'bug000', 'red');
 
 -- ----------------------------
 -- Table structure for t_sys_dictionary
@@ -260,15 +269,24 @@ CREATE TABLE `t_sys_dictionary` (
   UNIQUE KEY `CODE` (`CODE`),
   UNIQUE KEY `NAME` (`NAME`),
   UNIQUE KEY `CODE_2` (`CODE`),
+  UNIQUE KEY `UK_o1tl2dpxs4ogjrihj5d0aetyy` (`CODE`),
+  UNIQUE KEY `UK_a537d5dca8d3425ca9dda698d10` (`CODE`),
+  UNIQUE KEY `UK_f5f933daadac4883853243aad12` (`CODE`),
+  UNIQUE KEY `UK_e6c25999b7314e5181998f776ba` (`CODE`),
+  UNIQUE KEY `UK_8f14db30926d45208b68602823a` (`CODE`),
+  UNIQUE KEY `UK_60c6a856fe064bc68cfe0aee53b` (`CODE`),
   KEY `FK79C52CB373CC8B3F` (`DICTIONARYTYPE_CODE`),
   KEY `FK79C52CB3BD49F8CB` (`PARENT_CODE`),
-  CONSTRAINT `FK79C52CB3BD49F8CB` FOREIGN KEY (`PARENT_CODE`) REFERENCES `t_sys_dictionary` (`CODE`),
-  CONSTRAINT `FK79C52CB373CC8B3F` FOREIGN KEY (`DICTIONARYTYPE_CODE`) REFERENCES `t_sys_dictionarytype` (`CODE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `FK79C52CB373CC8B3F` FOREIGN KEY (`DICTIONARYTYPE_CODE`) REFERENCES `t_sys_dictionarytype` (`CODE`),
+  CONSTRAINT `FK79C52CB3BD49F8CB` FOREIGN KEY (`PARENT_CODE`) REFERENCES `t_sys_dictionary` (`CODE`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_sys_dictionary
 -- ----------------------------
+INSERT INTO `t_sys_dictionary` VALUES ('3', '2014-03-29 17:49:10', 'admin', '0', null, null, '0', 'bug001', '功能性', '1', '', 'bug001', 'bug', null);
+INSERT INTO `t_sys_dictionary` VALUES ('4', '2014-03-29 17:49:24', 'admin', '0', null, null, '0', 'bug002', '建议', '2', '', 'bug002', 'bug', null);
+INSERT INTO `t_sys_dictionary` VALUES ('5', '2014-03-29 20:27:09', 'admin', '0', null, null, '0', 'bug000', '内部新闻', '3', '', 'bug000', 'bug', null);
 
 -- ----------------------------
 -- Table structure for t_sys_dictionarytype
@@ -291,10 +309,45 @@ CREATE TABLE `t_sys_dictionarytype` (
   UNIQUE KEY `CODE` (`CODE`),
   UNIQUE KEY `NAME` (`NAME`),
   UNIQUE KEY `CODE_2` (`CODE`),
+  UNIQUE KEY `UK_9als5rp87ewbp9egotvj1j9rg` (`CODE`),
+  UNIQUE KEY `UK_753186875d0243ee833be0fb462` (`CODE`),
+  UNIQUE KEY `UK_2064ba05f4df4d2cb4dd86255b5` (`CODE`),
+  UNIQUE KEY `UK_2281184778e548688543cc82330` (`CODE`),
+  UNIQUE KEY `UK_551c03d742b847f8a4f1d44527e` (`CODE`),
+  UNIQUE KEY `UK_02b721e6df5f42169f82ffab7bf` (`CODE`),
   KEY `FK8551226D4DC80EF0` (`GROUP_CODE`),
   CONSTRAINT `FK8551226D4DC80EF0` FOREIGN KEY (`GROUP_CODE`) REFERENCES `t_sys_dictionarytype` (`CODE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_sys_dictionarytype
+-- ----------------------------
+INSERT INTO `t_sys_dictionarytype` VALUES ('5', '2014-03-29 17:48:24', 'admin', '0', null, null, '0', 'system', '系统字典', '1', '', null);
+INSERT INTO `t_sys_dictionarytype` VALUES ('6', '2014-03-29 17:48:52', 'admin', '0', null, null, '0', 'bug', 'bug类型', '2', '', 'system');
+
+-- ----------------------------
+-- Table structure for t_sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_log`;
+CREATE TABLE `t_sys_log` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(36) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `UPDATE_TIME` datetime DEFAULT NULL,
+  `UPDATE_USER` varchar(36) DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  `ACTION` varchar(255) DEFAULT NULL,
+  `ACTION_TIME` varchar(20) DEFAULT NULL,
+  `IP` varchar(64) DEFAULT NULL,
+  `LOGIN_NAME` varchar(36) DEFAULT NULL,
+  `MODULE` varchar(36) DEFAULT NULL,
+  `OPER_TIME` datetime DEFAULT NULL,
+  `REMARK` varchar(255) DEFAULT NULL,
+  `TYPE` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_sys_log
 -- ----------------------------
